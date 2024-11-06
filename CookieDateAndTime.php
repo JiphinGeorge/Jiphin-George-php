692423e6f19d91784e9b78b1447a2027faad4254
JiphinGeorge <jiphingeorge80@gmail.com>
2024-11-06 11:56:49 +0530

    okey

--- CookieDateAndTime.php
+++ CookieDateAndTime.php
@@ -1,14 +1,17 @@
 <html>
 <body>
-    <h2> Last visited time on the web page</h2>
+    <h2>Last visited time on the web page</h2>
     <?php
-    $intm = 60 * 60 * 24 * 60 + time();
+    
+    $intm = 60 * 60 * 24 * 60 + time();  // 60 days in seconds
     setcookie('lastVisit', date("G:i - m/d/y"), $intm);
+
     if (isset($_COOKIE['lastVisit'])) {
         $visit = $_COOKIE['lastVisit'];
         echo "Your last visit was - " . $visit;
-    } else
+    } else {
         echo "You have not visited this website before!";
+    }
     ?>
 </body>
 </html>
--- Upload/upload.php
+++ Upload/upload.php
@@ -1,27 +1,51 @@
 <?php
 $target_dir = "Upload/";
-$target_file = $target_dir .basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
-$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-// Check if image file is a actual image or fake image
-if(isset($_POST["submit"])) {
-  $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-  if($check !== false) {
-    echo "File is an image - " . $check["mime"] . ".";
-    $uploadOk = 1;
-  } else {
-    echo "File is not an image.";
+$imageFileType = strtolower(pathinfo($_FILES["fileToUpload"]["name"], PATHINFO_EXTENSION));
+
+// Check if file was uploaded without errors
+if ($_FILES["fileToUpload"]["error"] != 0) {
+    echo "Error: " . $_FILES["fileToUpload"]["error"];
     $uploadOk = 0;
-  }
 }
+
+// Check if file is an image
+if (isset($_POST["submit"])) {
+    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+    if ($check !== false) {
+        echo "File is an image - " . $check["mime"] . ".<br>";
+        $uploadOk = 1;
+    } else {
+        echo "File is not an image.<br>";
+        $uploadOk = 0;
+    }
+}
+
+// Limit allowed file types (only image files)
+$allowedTypes = ['jpg', 'jpeg', 'png', 'gif'];
+if (!in_array($imageFileType, $allowedTypes)) {
+    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.<br>";
+    $uploadOk = 0;
+}
+
+// Limit file size to 5MB
+if ($_FILES["fileToUpload"]["size"] > 5000000) { // 5MB
+    echo "Sorry, your file is too large.<br>";
+    $uploadOk = 0;
+}
+
+// Check if $uploadOk is set to 0 by an error
 if ($uploadOk == 0) {
-  echo "Sorry, your file was not uploaded.";
-// if everything is ok, try to upload file
+    echo "Sorry, your file was not uploaded.<br>";
 } else {
-  if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-    echo "The file has been uploaded.";
-  } else {
-    echo "Sorry, there was an error uploading your file.";
-  }
+    // Generate a unique file name to avoid overwriting
+    $target_file = $target_dir . uniqid('img_', true) . "." . $imageFileType;
+
+    // Try to upload file
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+        echo "The file has been uploaded to: " . $target_file . "<br>";
+    } else {
+        echo "Sorry, there was an error uploading your file.<br>";
+    }
 }
 ?>
